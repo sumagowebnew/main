d93211863ead8cf1dfc7e7de62c5728fe2f6af1b
mayulondhe16 <mayulondhe16@gmail.com>
2023-11-06 14:18:04 +0530

    career enquiries date issue

--- app/Http/Controllers/CareerEnquiriesController.php
+++ app/Http/Controllers/CareerEnquiriesController.php
@@ -25,9 +25,8 @@ class CareerEnquiriesController extends Controller
             // $data['mobile_no'] = $data['mobile_no'];
             // $data['technology_choice'] = $data['technology_choice'];
             // $data['position'] = $data['position'];
-            $data['date'] = $data['created_at']->toDateString();
+            $data['date'] = $data['created_at'];
             // $data['duration'] = $data['duration'];
-
             $cv = $data['cv'];
             $imagePath ="uploads/cv_files/" . $cv;
             $base64 = "data:application/pdf;base64," . base64_encode(file_get_contents($imagePath));
